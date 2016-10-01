2b8043566a571c3aa749ffbd916348726b8d7033
masutokw <masutokw@users.noreply.github.com>
2016-10-02 01:05:13 +0200

    Just cleaned

--- main.c
+++ main.c
@@ -5,10 +5,14 @@
 #include <libopencm3/stm32/exti.h>
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/cm3/scb.h>
+#include <libopencm3/stm32/rtc.h>
+#include <time.h>
+//#include <libopencm3/stm32/pwr.h>
 #include "taki.h"
 #include "motor.h"
 #include "nexstar.h"
 #include "usb_cdc.h"
+#include "mount.h"
 
 /*extern char rx_str[128];
 extern int rx_str_len;
@@ -25,13 +29,10 @@ extern long counter_x,counter_y;
 extern double res_x,res_y,speed_x;
 extern int dir_x,dir_y;*/
 int sync_cmd=0;
-uint16_t compare_time,compare_time3;
-uint16_t new_time,new_time3;
+uint16_t compare_time2,compare_time3;
+uint16_t new_time2,new_time3;
 uint16_t frequency,frequency2;
 int tracedata = 0;
-c_star st_target,
-       st_next,
-       st_current;
 c_double now;
 uint64_t Sys_Ticks;
 static void clock_setup(void)
@@ -113,13 +114,8 @@ static void tim_setup(void)
 
 static void tim_setup3(void)
 {
-    /* Enable TIM2 clock. */
     rcc_periph_clock_enable(RCC_TIM3);
-
-    /* Enable TIM2 interrupt. */
     nvic_enable_irq(NVIC_TIM3_IRQ);
-
-    /* Reset TIM2 peripheral. */
     timer_reset(TIM3);
 
     /* Timer global mode:
@@ -132,16 +128,9 @@ static void tim_setup3(void)
 
     /* Reset prescaler value. */
     timer_set_prescaler(TIM3, 720);
-
-    /* Enable preload. */
     timer_disable_preload(TIM3);
-
-    /* Continous mode. */
     timer_continuous_mode(TIM3);
-
-
     timer_set_period(TIM3, 65535);
-
     /* Disable outputs. */
     timer_disable_oc_output(TIM3, TIM_OC1);
     timer_disable_oc_output(TIM3, TIM_OC2);
@@ -201,14 +190,14 @@ void tim2_isr(void)
          * Get current timer value to calculate next
          * compare register value.
          */
-        compare_time = timer_get_counter(TIM2);
+        compare_time2 = timer_get_counter(TIM2);
 
         /* Calculate and set the next compare value. */
-        new_time = compare_time + ticks_x;
-        timer_set_oc_value(TIM2, TIM_OC1, new_time);
+        new_time2 = compare_time2 + ticks_x;
+        timer_set_oc_value(TIM2, TIM_OC1, new_time2);
         counter_x+=dir_x;
         //do step
-        gpio_toggle(GPIOC, GPIO14);
+        gpio_toggle(AZ_PORT,AZ_PIN);
     }
 }
 void tim3_isr(void)
@@ -233,7 +222,7 @@ void tim3_isr(void)
         timer_set_oc_value(TIM3, TIM_OC1, new_time3);
         counter_y+=dir_y;
         /* do_step */
-        gpio_toggle(GPIOC, GPIO13);
+        gpio_toggle(ALT_PORT,ALT_PIN);
 
     }
 }
@@ -257,60 +246,16 @@ void gotoxy(int x,int y)
     printf("%c[%d;%df",0x1B,y,x);
 }
 
-/*parses nexstar received string ,fired by USB RX interrupt callback  function*/
+/*Parse nexstar´s received string ,fired by USB RX interrupt callback  function
 void nexstar_poll(void)
 {
     nexstar_init();
     nexstar_cmd(rx_str,rx_str_len);
     rx_str_len=0;
     cdcacm_print_buff(response,resp_size);
-}
+}*/
 
-/* fired by Systick timer 100ms computes horizontal coordinates from ecuatorial target
- and corrected speed for both axes by using Taki´s matrix transforms*/
 
-void track(void)
-{
-    st_target.timer_count=st_current.timer_count=( Sys_Ticks/1000.0);
-    //compute correct actual alt-az mount values
-    to_alt_az(&st_target);
-    //translate counter values to current postition alt/az values
-    st_current.az=(res_x*counter_x)/RAD_TO_ARCS;
-    st_current.alt=(res_y*counter_y)/RAD_TO_ARCS;
-    //compute ecuatorial current equatorial values to be send out from NEXSTAR protocol interface
-    to_equatorial(&st_current);
-    //compute next alt/az mount values  for target next lap second
-    st_next.timer_count= st_target.timer_count+1.0;
-    to_alt_az(&st_next);
-
-    c_double d_az= RAD_TO_ARCS*(st_next.az-st_target.az);
-    c_double d_alt=RAD_TO_ARCS*(st_next.alt-st_target.alt);
-
-    float temp=st_target.az*RAD_TO_DEG;
-    float temp1=st_target.alt*RAD_TO_DEG;
-
-    //compute delta values :next values from actual values for desired target coordinates
-    c_double d_az_r=RAD_TO_ARCS*(st_next.az)-res_x*counter_x;
-    c_double d_alt_r=RAD_TO_ARCS*(st_next.alt)-res_x*counter_y;
-    //if trace print computed   stuff  to USB output stream
-    if (tracedata)
-    {
-        gotoxy(0,4);
-        printf("Time :%2.3fs  %2.4f  %2.4f  %2.4f %2.4f \r\n ",Sys_Ticks/1000.0,temp,temp1, d_az,d_alt );
-        printf("Delta %+4.2f %+4.2f \r\n",temp*3600.0-res_x*counter_x, temp1*3600.0-res_y*counter_y );
-        printf("Freq %2.4f %2.4f\r\n",d_az/res_x,d_alt/res_y);
-        //to_equatorial(&st_r);
-       //
-        printf("Freq %2.4f %2.4f %f\r\n",d_az_r/res_x,d_alt_r/res_y,speed_x);
-    };
-
-    // Computes and timer intervals for stepper  rates
-   // speedy(d_alt_r);
-  //  speedx(d_az_r);
-    speed_x_a(d_az_r);
-    speed_y_a(d_alt_r);
-
-}
 
 
 //}
@@ -330,7 +275,6 @@ int main(void)
 
     for ( i = 0; i < 0x10000; i++)
         __asm__("nop");
-
     cdcacm_init();
     gpio_setup();
     systick_setup();
@@ -342,10 +286,13 @@ int main(void)
     to_alt_az(&st_target);
     counter_x=st_target.az*RAD_TO_ARCS/res_x;
     counter_y=st_target.alt*180.0*3600.0/(M_PI*res_y);
+    rtc_auto_awake(RCC_HSE, 0xf424);
+    //
     while (1)
-    {   if (rx_str_len) nexstar_poll();//procces nexstar commnad if any
+    {
+        if (rx_str_len) nexstar_poll();//procces nexstar commnad if any
         if ((Sys_Ticks%100)==0) track();//track loop
-
     }
     return 0;
 }
+//@param[in] clock_source ::rcc_osc. RTC clock source. Only HSE, LSE    and LSI are permitted.
